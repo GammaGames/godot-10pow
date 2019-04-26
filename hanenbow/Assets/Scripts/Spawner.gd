f9c2e4a533064fb3d2e75874bd6d9d698a8e24f8
GammaGames <jesse007@ymail.com>
2019-04-25 22:22:00 -0600

    add restart key and small refactor

--- hanenbow/Assets/Scripts/Spawner.gd
+++ hanenbow/Assets/Scripts/Spawner.gd
@@ -30,41 +30,9 @@ func set_sound(sound):
 func _timeout():
     var new = ball.duplicate()
     new.call_deferred("set_sound", sound)
-    new.notes = gen_notes()
     new.vel = Vector2(4, 0).rotated(deg2rad(angle+45))
     $"..".add_child(new)
     new.position = position
 
     angle += 90
     angle %= 360
-
-
-func gen_notes():
-    var notes = []
-    #seed(hash(angle+1))
-    seed(3) # seed = 2 is nice
-    randi()
-
-    var base_pitch = 0.06
-    var note1 = pow(2, 1 + randi() % 12/12.0)
-    var note2 = pow(2, 1 + randi() % 12/12.0)
-    var note3 = pow(2, 1 + randi() % 12/12.0)
-
-    notes.push_back(2 * base_pitch)
-    notes.push_back(note1 * base_pitch)
-    notes.push_back(note2 * base_pitch)
-    notes.push_back(note3 * base_pitch)
-
-    for i in 100:
-        var n1 = notes[-1]
-        var n2 = notes[-2]
-        var n3 = notes[-3]
-        var n4 = notes[-4]
-
-        notes.push_back(n1 * 2)
-        notes.push_back(n2 * 2)
-        notes.push_back(n3 * 2)
-        notes.push_back(n4 * 2)
-
-    randomize()
-    return notes
